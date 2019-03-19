6518b1a0a5d9ae97b941450e16a90d4e9aaff255
Gianluca Nitti <gianluca.nitti96@gmail.com>
2019-03-19 16:37:24 +0100

    Cleanup

--- ebnf_parser.py
+++ ebnf_parser.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python3
 
-# source: https://karmin.ch/ebnf/index
-# TODO: usare NodeVisitor
 ebnf_in_ebnf = '''
-syntax     = production+
+syntax = production+
 production = identifier ws \"::=\" expression ( \".\" / \";\" ) ws
 expression = ws term ( ws \"|\" term )* ws
-term       = (ws factor)+
-factor     = (identifier)
-           / (literal)
-           / (optional)
-           / (\"(\" expression \")\")
-           / (list)
+term = (ws factor)+
+factor = identifier / literal / optional / list
 optional = \"[\" expression \"]\"
 list = \"{\" expression \"}\"
 identifier = ~\"[a-zA-Z0-9_]+\"
-title      = literal
-comment    = literal
-literal    = ~\"\\\".*?\\\"\"
-ws         = ~\"\s*\"
+literal = ~\"\\\".*?\\\"\"
+ws = ~\"\s*\"
 '''
 
-import itertools, parsimonious
+import parsimonious
 
-ebnf_grammar = parsimonious.Grammar(ebnf_in_ebnf)
-with open('vhdl.ebnf', 'r') as f:
-    input_ebnf = f.read()
-
-class Ident:
-    def __init__(self, ident):
-        self.ident = ident
+class Production:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
     def pp(self):
-        return 'Ident( ' + self.ident + ' )'
-class Literal:
-    def __init__(self, literal):
-        self.literal = literal
+        return self.lhs + ' ::= ' + self.rhs.pp()
+class Alternative:
+    def __init__(self, alts):
+        self.alts = alts
     def pp(self):
-        return 'Literal( ' + self.literal + ' )'
+        return 'Alternative( ' + ', '.join(map(lambda x: x.pp(), self.alts)) + ' )'
+class Sequence:
+    def __init__(self, parts):
+        self.parts = parts
+    def pp(self):
+        return 'Sequence( ' + ', '.join(map(lambda x: x.pp(), self.parts)) + ' )'
 class Optional:
     def __init__(self, content):
         self.content = content
@@ -47,35 +41,30 @@ class List:
         self.content = content
     def pp(self):
         return 'List( ' + self.content.pp() + ' )'
-class Sequence:
-    def __init__(self, parts):
-        self.parts = parts
+class Identifier:
+    def __init__(self, ident):
+        self.ident = ident
     def pp(self):
-        return 'Sequence( ' + ', '.join(map(lambda x: x.pp(), self.parts)) + ' )'
-class Alternative:
-    def __init__(self, alts):
-        self.alts = alts
+        return 'Identifier( ' + self.ident + ' )'
+class Literal:
+    def __init__(self, literal):
+        self.literal = literal
     def pp(self):
-        return 'Alternative( ' + ', '.join(map(lambda x: x.pp(), self.alts)) + ' )'
-class Rule:
-    def __init__(self, lhs, rhs):
-        self.lhs = lhs
-        self.rhs = rhs
-    def pp(self):
-        return self.lhs + ' ::= ' + self.rhs.pp()
+        return 'Literal( ' + self.literal + ' )'
+
 class EbnfASTVisitor(parsimonious.NodeVisitor):
     def generic_visit(self, node, children):
-        return (node.text, children, node.expr_name) #node.text
+        return (node.text, children, node.expr_name)
     def visit_syntax(self, node, children):
         return children # list of productions
     def visit_production(self, node, children):
         children = self.nospaces(children)
         assert len(children) == 4
-        assert isinstance(children[0], Ident)
+        assert isinstance(children[0], Identifier)
         assert children[1][0] == '::='
         # assert isinstance(children[2], Sequence)
         assert children[3][0] in [';', '.']
-        return Rule(children[0].ident, children[2])
+        return Production(children[0].ident, children[2])
     def visit_expression(self, node, children):
         children = self.nospaces(children)
         assert len(children) == 2
@@ -104,7 +93,7 @@ class EbnfASTVisitor(parsimonious.NodeVisitor):
         assert len(children) == 3 and children[0][0] == '{' and children[2][0] == '}'
         return List(children[1])
     def visit_identifier(self, node, children):
-        return Ident(node.text)
+        return Identifier(node.text)
     def visit_literal(self, node, children):
         return Literal(node.text)
     def visit_ws(self, node, children):
@@ -112,7 +101,11 @@ class EbnfASTVisitor(parsimonious.NodeVisitor):
     def nospaces(self, nodes):
         return list(filter(lambda x: x != None, nodes))
 
-parseTree = ebnf_grammar.parse(input_ebnf)
-ast = EbnfASTVisitor().visit(parseTree)
-for prod in ast:
-    print(prod.pp())
+if __name__ == '__main__':
+    ebnf_grammar = parsimonious.Grammar(ebnf_in_ebnf)
+    with open('vhdl.ebnf', 'r') as f:
+        input_ebnf = f.read()
+    parseTree = ebnf_grammar.parse(input_ebnf)
+    ast = EbnfASTVisitor().visit(parseTree)
+    for prod in ast:
+        print(prod.pp())
